04b6145aa2b97381e7c4bfa4d3860f7a3b57a7e7
EnriqueOtazu <otazu02@gmail.com>
2023-09-05 17:23:11 -0300

    header de index

--- novabank/src/components/Index/Index.js
+++ novabank/src/components/Index/Index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import logo from "./NovaBank.png";
-// import './Index.css';
 import {Navbar,Nav,NavItem,NavLink,NavbarBrand} from 'reactstrap';
 
 export const Index = () => {
   return (
     <>
-      <Navbar className="my-1" color="light">
+      <Navbar className="my-1," color="faded" style={{boxShadow:' 0px 0.5px 0px 0px rgba(0,0,0,0.3)'}}>
         <NavbarBrand href="/">
-          <img alt="logo" src={logo}style={{height: 40, width: 180}}/>
+          <img alt="logo" src={logo} style={{height: 40, width: 180}} block/>
         </NavbarBrand>
-        <Nav className="mr-auto" navbar>
+        <Nav className="mr-auto" style={{color:'#3044B6'}}>
+
           <NavItem>
-            <NavLink href="/components/" className='mr-auto' Navbar>Components</NavLink>
+            <NavLink href="/components/"  Navbar>Ayuda</NavLink>
           </NavItem>
+
+          <NavItem left>
+            <NavLink href="/components/"  Navbar>mi cuenta</NavLink>
+          </NavItem>
+
         </Nav>
       </Navbar>
     </>
