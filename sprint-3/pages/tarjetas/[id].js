0264fcac34b34555b7449e12fed2fbab49f4facb
iaradg <iaradg123@gmail.com>
2023-09-23 15:35:23 -0300

    tarjetas

--- sprint-3/pages/tarjetas/[id].js
+++ sprint-3/pages/tarjetas/[id].js
@@ -3,6 +3,8 @@ import tarjetasData from './tarjetasData';
 import { useState } from 'react';
 import resumenData from './resumenData';
 import ResumenTabla from '@/components/ResumenTabla';
+import Image from 'next/image';
+
 
 function TarjetaDetail({ tarjeta }) {
   const router = useRouter();
@@ -50,12 +52,16 @@ function TarjetaDetail({ tarjeta }) {
 
   return (
     <div>
-      <h1>Tarjeta de Crédito</h1>
-      <p>Número de Tarjeta: {tarjeta.numeroTarjeta}</p>
-      <p>Fecha de Creación: {tarjeta.fechaCreacion}</p>
-      <p>Fecha de Vencimiento: {tarjeta.fechaVencimiento}</p>
-      <p>Código de Seguridad: {tarjeta.codigoSeguridad}</p>
-      <p>Nombre del Titular: {tarjeta.nombreTitular}</p>
+        <h1 style={{}}>Tarjeta de Crédito</h1>
+      <div className="credit-card" style={{ boxShadow: '-4px 4px 12px 0px rgba(0,0,0,0.5)' }}>
+      <Image src="/public/NovaBank.png" alt="Tarjeta de Crédito" className="credit-card-image" width={300} height={200}/>
+        <p className="credit-card-detail"><b>Número de Tarjeta:</b> {tarjeta.numeroTarjeta}</p>
+        <p className="credit-card-detail"><b>Fecha de Creación:</b> {tarjeta.fechaCreacion}</p>
+        <p className="credit-card-detail"><b>Fecha de Vencimiento:</b> {tarjeta.fechaVencimiento}</p>
+        <p className="credit-card-detail"><b>Código de Seguridad:</b> {tarjeta.codigoSeguridad}</p>
+        <p className="credit-card-detail"><b>Nombre del Titular:</b> {tarjeta.nombreTitular}</p>
+    </div>
+
       <button onClick={solicitarAumentoLimite}>Solicitar Aumento de Límite</button>
       <button onClick={obtenerResumen}>Obtener Resumen</button>
       {mostrarResumen && <ResumenTabla realizarPago={realizarPago} />}
